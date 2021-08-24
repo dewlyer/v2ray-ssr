ba0aac5e42320a9d0ab3daf3028f5878a66a9717
dewlyer <dewlyer@gmail.com>
2021-08-24 16:54:01 +0800

    project update

--- libs/services.js
+++ libs/services.js
@@ -1,41 +1,28 @@
 const crawler = require('../libs/crawler');
 
+function updateSourceCallback(all, cb) {
+  let list = [];
+  all.forEach(({name, result}) => {
+    list = list.concat(result.map((item, index) => {
+      const id = String(index + 1).padStart(3, '0');
+      return {
+        name: `${name}_${id}`,
+        url: item || ''
+      }
+    }));
+    // list.sort((a, b) => (a.name.slice(-3) - b.name.slice(-3)));
+  });
+  cb && cb(list)
+}
+
 function updateSourceData(cb) {
   console.log('source sync begin ==> ' + new Date().toLocaleString());
-  const callback = (all) => {
-    let list = [];
-    all.forEach(({name, result}) => {
-      list = list.concat(result.map((item, index) => {
-        const id = String(index + 1).padStart(3, '0');
-        return {
-          name: `${name}_${id}`,
-          url: item || ''
-        }
-      }));
-      // list.sort((a, b) => (a.name.slice(-3) - b.name.slice(-3)));
-    });
-    cb && cb(list)
-  };
-  crawler.getData(callback);
+  crawler.getData((all) => updateSourceCallback(all, cb));
 }
 
 function updateSourceDataWithProxy(cb) {
   console.log('source sync with proxy begin ==> ' + new Date().toLocaleString());
-  const callback = (all) => {
-    let list = [];
-    all.forEach(({name, result}) => {
-      list = list.concat(result.map((item, index) => {
-        const id = String(index + 1).padStart(3, '0');
-        return {
-          name: `${name}_${id}`,
-          url: item || ''
-        }
-      }));
-      // list.sort((a, b) => (a.name.slice(-3) - b.name.slice(-3)));
-    });
-    cb && cb(list)
-  };
-  crawler.getData(callback, true);
+  crawler.getData((all) => updateSourceCallback(all, cb), true);
 }
 
 module.exports = {
